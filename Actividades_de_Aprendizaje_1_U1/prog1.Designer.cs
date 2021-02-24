0f2904d0ae875c63780a32d358d2de640cb8140d
Victor Hugo Carreon Pulido <hv_carreon@hotmail.com>
2021-02-24 15:06:01 -0600

    Se añadieron funcionalidades al dibujado de la figura, trackbar para posicion y tamaño.

--- Actividades_de_Aprendizaje_1_U1/prog1.Designer.cs
+++ Actividades_de_Aprendizaje_1_U1/prog1.Designer.cs
@@ -41,8 +41,20 @@ namespace Actividades_de_Aprendizaje_1_U1
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.trcBrX = new System.Windows.Forms.TrackBar();
+            this.trcBrY = new System.Windows.Forms.TrackBar();
+            this.trcBrAncho = new System.Windows.Forms.TrackBar();
+            this.trcBrAlto = new System.Windows.Forms.TrackBar();
+            this.lblX = new System.Windows.Forms.Label();
+            this.lblY = new System.Windows.Forms.Label();
+            this.lblAncho = new System.Windows.Forms.Label();
+            this.lblAlto = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pctAreaDibujo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.icnPctBoxFlecha)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trcBrX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trcBrY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trcBrAncho)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trcBrAlto)).BeginInit();
             this.SuspendLayout();
             // 
             // pctAreaDibujo
@@ -80,7 +92,7 @@ namespace Actividades_de_Aprendizaje_1_U1
             // 
             // txtCoordX
             // 
-            this.txtCoordX.Location = new System.Drawing.Point(12, 359);
+            this.txtCoordX.Location = new System.Drawing.Point(12, 362);
             this.txtCoordX.Name = "txtCoordX";
             this.txtCoordX.Size = new System.Drawing.Size(82, 20);
             this.txtCoordX.TabIndex = 3;
@@ -88,7 +100,7 @@ namespace Actividades_de_Aprendizaje_1_U1
             // lbXcoords
             // 
             this.lbXcoords.AutoSize = true;
-            this.lbXcoords.Location = new System.Drawing.Point(12, 343);
+            this.lbXcoords.Location = new System.Drawing.Point(12, 346);
             this.lbXcoords.Name = "lbXcoords";
             this.lbXcoords.Size = new System.Drawing.Size(14, 13);
             this.lbXcoords.TabIndex = 7;
@@ -96,21 +108,21 @@ namespace Actividades_de_Aprendizaje_1_U1
             // 
             // txtCoordY
             // 
-            this.txtCoordY.Location = new System.Drawing.Point(100, 359);
+            this.txtCoordY.Location = new System.Drawing.Point(100, 362);
             this.txtCoordY.Name = "txtCoordY";
             this.txtCoordY.Size = new System.Drawing.Size(82, 20);
             this.txtCoordY.TabIndex = 8;
             // 
             // txtAncho
             // 
-            this.txtAncho.Location = new System.Drawing.Point(188, 359);
+            this.txtAncho.Location = new System.Drawing.Point(188, 362);
             this.txtAncho.Name = "txtAncho";
             this.txtAncho.Size = new System.Drawing.Size(82, 20);
             this.txtAncho.TabIndex = 9;
             // 
             // txtAlto
             // 
-            this.txtAlto.Location = new System.Drawing.Point(276, 359);
+            this.txtAlto.Location = new System.Drawing.Point(276, 362);
             this.txtAlto.Name = "txtAlto";
             this.txtAlto.Size = new System.Drawing.Size(85, 20);
             this.txtAlto.TabIndex = 10;
@@ -118,7 +130,7 @@ namespace Actividades_de_Aprendizaje_1_U1
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(100, 343);
+            this.label1.Location = new System.Drawing.Point(100, 346);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(14, 13);
             this.label1.TabIndex = 11;
@@ -127,7 +139,7 @@ namespace Actividades_de_Aprendizaje_1_U1
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(188, 343);
+            this.label2.Location = new System.Drawing.Point(188, 346);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(38, 13);
             this.label2.TabIndex = 12;
@@ -136,17 +148,101 @@ namespace Actividades_de_Aprendizaje_1_U1
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(273, 343);
+            this.label3.Location = new System.Drawing.Point(273, 346);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(25, 13);
             this.label3.TabIndex = 13;
             this.label3.Text = "Alto";
             // 
+            // trcBrX
+            // 
+            this.trcBrX.Location = new System.Drawing.Point(12, 401);
+            this.trcBrX.Maximum = 349;
+            this.trcBrX.Name = "trcBrX";
+            this.trcBrX.Size = new System.Drawing.Size(349, 45);
+            this.trcBrX.TabIndex = 14;
+            this.trcBrX.Value = 10;
+            this.trcBrX.Scroll += new System.EventHandler(this.trcBrX_Scroll);
+            // 
+            // trcBrY
+            // 
+            this.trcBrY.Location = new System.Drawing.Point(12, 452);
+            this.trcBrY.Maximum = 232;
+            this.trcBrY.Name = "trcBrY";
+            this.trcBrY.Size = new System.Drawing.Size(349, 45);
+            this.trcBrY.TabIndex = 15;
+            this.trcBrY.Value = 10;
+            this.trcBrY.Scroll += new System.EventHandler(this.trcBrY_Scroll);
+            // 
+            // trcBrAncho
+            // 
+            this.trcBrAncho.Location = new System.Drawing.Point(12, 503);
+            this.trcBrAncho.Maximum = 349;
+            this.trcBrAncho.Name = "trcBrAncho";
+            this.trcBrAncho.Size = new System.Drawing.Size(349, 45);
+            this.trcBrAncho.TabIndex = 16;
+            this.trcBrAncho.Value = 10;
+            this.trcBrAncho.Scroll += new System.EventHandler(this.trcBrAncho_Scroll);
+            // 
+            // trcBrAlto
+            // 
+            this.trcBrAlto.Location = new System.Drawing.Point(12, 554);
+            this.trcBrAlto.Maximum = 232;
+            this.trcBrAlto.Name = "trcBrAlto";
+            this.trcBrAlto.Size = new System.Drawing.Size(349, 45);
+            this.trcBrAlto.TabIndex = 17;
+            this.trcBrAlto.Value = 10;
+            this.trcBrAlto.Scroll += new System.EventHandler(this.trcBrAlto_Scroll);
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Location = new System.Drawing.Point(162, 436);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(13, 13);
+            this.lblX.TabIndex = 18;
+            this.lblX.Text = "0";
+            // 
+            // lblY
+            // 
+            this.lblY.AutoSize = true;
+            this.lblY.Location = new System.Drawing.Point(162, 484);
+            this.lblY.Name = "lblY";
+            this.lblY.Size = new System.Drawing.Size(13, 13);
+            this.lblY.TabIndex = 19;
+            this.lblY.Text = "0";
+            // 
+            // lblAncho
+            // 
+            this.lblAncho.AutoSize = true;
+            this.lblAncho.Location = new System.Drawing.Point(162, 535);
+            this.lblAncho.Name = "lblAncho";
+            this.lblAncho.Size = new System.Drawing.Size(13, 13);
+            this.lblAncho.TabIndex = 20;
+            this.lblAncho.Text = "0";
+            // 
+            // lblAlto
+            // 
+            this.lblAlto.AutoSize = true;
+            this.lblAlto.Location = new System.Drawing.Point(162, 586);
+            this.lblAlto.Name = "lblAlto";
+            this.lblAlto.Size = new System.Drawing.Size(13, 13);
+            this.lblAlto.TabIndex = 21;
+            this.lblAlto.Text = "0";
+            // 
             // prog1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(374, 391);
+            this.ClientSize = new System.Drawing.Size(374, 621);
+            this.Controls.Add(this.lblAlto);
+            this.Controls.Add(this.lblAncho);
+            this.Controls.Add(this.lblY);
+            this.Controls.Add(this.lblX);
+            this.Controls.Add(this.trcBrAlto);
+            this.Controls.Add(this.trcBrAncho);
+            this.Controls.Add(this.trcBrY);
+            this.Controls.Add(this.trcBrX);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -165,6 +261,10 @@ namespace Actividades_de_Aprendizaje_1_U1
             this.Text = "Primer Programa";
             ((System.ComponentModel.ISupportInitialize)(this.pctAreaDibujo)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.icnPctBoxFlecha)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trcBrX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trcBrY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trcBrAncho)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trcBrAlto)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -183,5 +283,13 @@ namespace Actividades_de_Aprendizaje_1_U1
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trcBrX;
+        private System.Windows.Forms.TrackBar trcBrY;
+        private System.Windows.Forms.TrackBar trcBrAncho;
+        private System.Windows.Forms.TrackBar trcBrAlto;
+        private System.Windows.Forms.Label lblX;
+        private System.Windows.Forms.Label lblY;
+        private System.Windows.Forms.Label lblAncho;
+        private System.Windows.Forms.Label lblAlto;
     }
 }
